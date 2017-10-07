47366811255188d04d17d8546367c1fd81520b62
Mike Peiman <mikepeiman@gmail.com>
2017-10-07 15:51:34 -0700

    drafted logic/pseudocode and set up file structure

---
+++ app.js
@@ -0,0 +1,18 @@
+// 1. create array of transformer objects
+// 2. create transformer objects with stats, type and overall rating
+// 3. program takes input from user that defines a group of transformers, one by one
+// 4a. sort bots on each team for battle by rank
+// 4b. battle logic
+// 	if any bot is up 4+ courage, they win
+// 	if any bot is up 3+ strength, they win
+// 	if any bot is up 3+ skill, they win
+// 	default: the bot with the highest overall rating wins
+// 	in the event of a tie, both bots are considered destroyed
+// 	any extra bots on one team are skipped (ie. each bot only fights once)
+// 	any bot named Optimus Prime or Predaking automatically win their match, UNLESS they face each other, 
+// 	  and THEN all bots are destroyed and game ends
+// 5. battle output: how many battles, winning team, winning bot, survivors on losing team
+
+runApp = function() {
+
+}
